ff975220e22fdd424dcaa5c446cf5f5d4ce19166
Aytcavdep <96445044+Aytcavdep@users.noreply.github.com>
2022-08-25 16:33:56 +0300

    added component header, categories, pizzablock restyle html in react render component

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,30 @@
+import { useState } from "react";
+
 const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const onClickCategory = (index) => setActiveIndex(index);
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые"
+  ];
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categories.map((item, index) => {
+          return (
+            <li
+              className={activeIndex === index ? "active" : ""}
+              key={index}
+              onClick={() => onClickCategory(index)}
+            >
+              {item}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
