c8de4eb0c3b74555509a5f8c3e37d361044441fd
Коваленко Владимир <sazonov2380@gmail.com>
2022-08-29 05:45:59 +0300

    added flexsibility style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
-import "./scss/app.scss";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
+import './scss/app.scss';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -10,7 +12,11 @@ function App() {
         <Header />
         <div className="content">
           <div className="container">
-            <NotFound />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
           </div>
         </div>
       </div>
