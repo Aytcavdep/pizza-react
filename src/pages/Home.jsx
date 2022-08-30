ccc0d5aa452e9ad01dfe3799e3ff48a7bb2e4f8d
Aytcavdep <96445044+Aytcavdep@users.noreply.github.com>
2022-08-30 16:46:28 +0300

    Added search component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import PizzaBlock from "../components/PizzaBlock";
 import { useState, useEffect } from "react";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [categoryId, setCategoryId] = useState(0);
@@ -13,13 +13,14 @@ const Home = () => {
     sortProperty: "rating",
     sort: "desc"
   });
+  const search = searchValue ? `&searc=${searchValue}` : "";
 
   useEffect(() => {
     setIsLoading(true);
     fetch(
       `https://63082d44722029d9ddc94328.mockapi.io/items?${
         categoryId > 0 ? `category=${categoryId}` : ``
-      }&sortBy=${sortType.sortProperty}&order=${sortType.sort}`
+      }&sortBy=${sortType.sortProperty}&order=${sortType.sort}${search}`
     )
       .then((res) => res.json())
       .then((json) => {
@@ -28,6 +29,20 @@ const Home = () => {
         window.scrollTo(0, 0);
       });
   }, [categoryId, sortType]);
+
+  /* Фильтрация по существующему массиву
+  const pizzas = items.filter((obj) =>
+      obj.title.toLowerCase().includes(searchValue.toLowerCase())
+    ).map((pizza) => {
+      return <PizzaBlock key={pizza.id} {...pizza} />;
+    });
+    */
+  const pizzas = items.map((pizza) => {
+    return <PizzaBlock key={pizza.id} {...pizza} />;
+  });
+  const skeletons = [...new Array(6)].map((_, index) => (
+    <Skeleton key={index} />
+  ));
   return (
     <div className="container">
       <div className="content__top">
@@ -38,13 +53,7 @@ const Home = () => {
         <Sort value={sortType} onChangeSort={(id) => setSortType(id)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((pizza) => {
-              return <PizzaBlock key={pizza.id} {...pizza} />;
-            })}
-      </div>
+      <div className="content__items">{isLoading ? skeletons : pizzas}</div>
     </div>
   );
 };
