b027f5b411024b67bbd7b277af202bb0a49fb392
Aytcavdep <96445044+Aytcavdep@users.noreply.github.com>
2022-09-22 04:51:10 +0300

    added cart add pizzas logic process

--- src/redux/slices/cartSlice.js
+++ src/redux/slices/cartSlice.js
@@ -7,10 +7,25 @@ export const cartSlice = createSlice({
     items: []
   },
   reducers: {
-    addItem(state, action) {
+    /*addItem(state, action) {
       state.items.push(action.payload);
       state.totalPrice = state.items.reduce(
         (sum, obj) => (sum += obj.price),
+        0
+      );
+    },*/
+    addItem(state, action) {
+      const findItem = state.items.find((obj) => obj.id === action.payload.id);
+      if (findItem) {
+        findItem.count++;
+      } else {
+        state.items.push({
+          ...action.payload,
+          count: 1
+        });
+      }
+      state.totalPrice = state.items.reduce(
+        (sum, obj) => (sum += obj.price * obj.count),
         0
       );
     },
